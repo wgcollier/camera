51010905ec5aaca0504e46b1a6180c9745e4c7fd
bill <wgcollie@gmail.com>
2024-07-27 12:56:07 +0000

    Updates done by bill

---
+++ build/src/CMakeFiles/MYQHYCCDDEMO.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MYQHYCCDDEMO.dir/main.cpp.o"
+  "CMakeFiles/MYQHYCCDDEMO.dir/main.cpp.o.d"
+  "MYQHYCCDDEMO"
+  "MYQHYCCDDEMO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MYQHYCCDDEMO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
